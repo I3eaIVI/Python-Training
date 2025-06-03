5b5d9edf51a1fc90b4e48a841fe19e04773576b7
I3eaIVI <i3eaivi6906@gmail.com>
2025-06-03 14:32:00 +0700

    Create blabla2.py

---
+++ Pre-pro68/blabla2.py
@@ -0,0 +1,12 @@
+'''comment'''
+def main():
+    '''com'''
+    print("x.x.s,ds,x,f,w,34,43,d,2,d,34,d,43,d,d,hf,g,3")
+    print(f"{'smain()inthai----good-x+y=500ad,@#$*&ohs':>42}")
+    print(f"{'float()inlardkrangbang--gg -goodgame':>40}")
+    print(f"{'x*error=?\\what?-is-that-4%$#%67=':>38}")
+    print(f"{'j=i for i in range(90) = some':>37}")
+    print(f"{'int+str=floatif-else=IT21':>35}\n{'welcome to IT nakrub#':>33}")
+    print(f"{'Pre-pro is good$$':>31}\n{'gnlks+===$$%#@':>30}")
+    print(f"{'keepflight':>28}\n{'IT21$$':>26}\n{'OK!':>25}\n")
+main()
