f4c079ba6bc0e479c1e925a938467ed1618b9ad4
I3eaIVI <i3eaivi6906@gmail.com>
2025-06-03 14:32:29 +0700

    Create Sport.py

---
+++ Pre-pro68/Sport.py
@@ -0,0 +1,8 @@
+"""Sport"""
+def main():
+    """input sport"""
+    sport1 = str(input())
+    sport2 = str(input())
+    sport3 = str(input())
+    print(f"Today's activities are: {sport1 * 3}, {sport2 * 2}, {sport3 * 5}")
+main()
