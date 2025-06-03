fbefe31466abe1200d1eda95982ed6a66db29f8f
I3eaIVI <i3eaivi6906@gmail.com>
2025-06-03 14:32:08 +0700

    Create Escape the Backslash.py

---
+++ Pre-pro68/Escape the Backslash.py
@@ -0,0 +1,6 @@
+'''Escape the Backslash'''
+def main():
+    '''Escape from python'''
+    print("Loading...\\Please\\ wait\\ while\\ we\\ pretend\\ to\\ do"
+    "\\ something\\ important...\\Just\\ kidding.")
+main()
