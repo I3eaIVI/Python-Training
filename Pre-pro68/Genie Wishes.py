e23901f7bd00a7017a605e532aca92c1515f034c
I3eaIVI <i3eaivi6906@gmail.com>
2025-06-03 14:32:16 +0700

    Create Genie Wishes.py

---
+++ Pre-pro68/Genie Wishes.py
@@ -0,0 +1,8 @@
+'''Genie'''
+def main():
+    '''Wishes'''
+    first = str(input(""))
+    second = str(input(""))
+    third = str(input(""))
+    print(first, second, third, "All your wishes shall now come true! ",sep = ", ",end= "*Poooof*")
+main()
